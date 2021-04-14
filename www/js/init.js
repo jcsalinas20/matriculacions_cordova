554740c1b7c9ba85a95e871214225c8df9e1a5cf
jcsalinas20 <jcsalinasn20@gmail.com>
2021-04-14 19:13:17 +0200

    docs tab finished

--- www/js/init.js
+++ www/js/init.js
@@ -1,9 +1,13 @@
+let types = null;
+
 (async function () {
     $('.ui.accordion').accordion();
     initializeButtonsDashboard();
+    eventOnChangeSelected();
+    addEventsToDocsUploads();
 
-    const response = await getJson();
-
+    const career = await getJson();
+    types = await getTypes();
 })(jQuery);
 
 document.addEventListener('deviceready', onDeviceReady, false);
@@ -18,6 +22,81 @@ function onDeviceReady() {
 async function getJson() {
     return await fetch("/js/aws.json").then((res) => {
         return res.json();
+    })
+}
+
+async function getTypes() {
+    return await fetch("/js/types.json").then((res) => {
+        return res.json();
+    })
+}
+
+function eventOnChangeSelected() {
+    $("#documentation-tab div.select select").on("change", function () {
+        var selected = this.value;
+        $("#documentation-tab>.ui.list").html('');
+        for (let i = 0; i < types[selected].length; i++) {
+            $("#documentation-tab>.ui.list").append(`            
+        <div class="item row">
+            <div class="icon-list col s2 center">
+                <i class="${types[selected+"_icons"][i]} icon circular large"></i>
+            </div>
+            <div class="content col s10">
+                <div class="header">${types[selected][i]}</div>
+                <div class="body row">
+                    <p class="text col s2">Estat: </p>
+                    <div class="semaphore col s5">
+                        <i class="circle red icon"></i>
+                        <i class="circle orange outline icon"></i>
+                        <i class="circle green outline icon"></i>
+                    </div>
+                    <div class="btn-icons col s5">
+                        <div class="icon-eye">
+                            <i class="eye icon big blue"></i>
+                        </div>
+                        <div class="icon-camera">
+                            <i class="camera icon big blue"></i>
+                        </div>
+                        <label class="icon-upload" for="file-upload${i}">
+                            <input type="file" style="display: none;" id="file-upload${i}">
+                            <i class="upload icon big blue"></i>
+                        </label>
+                    </div>
+                </div>
+            </div>
+        </div>`);
+        }
+        addEventsToDocsUploads();
+    });
+}
+
+function addEventsToDocsUploads() {
+    $("#documentation-tab .item .icon-camera").on("click", (e) => {
+        navigator.camera.getPicture(onSuccess, onFail, {
+            quality: 50,
+            destinationType: Camera.DestinationType.DATA_URL
+        });
+
+        function onSuccess(imageData) {
+            let semaphore = $(e.target).closest("div.body").children(".semaphore");
+            semaphore.children().addClass("outline");
+            semaphore.children(".orange").removeClass("outline");
+            $(e.target).closest("div.btn-icons").children("img").prop("src", "data:image/jpeg;base64," + imageData);
+        }
+
+        function onFail(message) {
+            alert('Failed because: ' + message);
+        }
+    })
+    $("#documentation-tab .item .icon-eye").on("click", (e) => {
+        console.log("Developing :)");
+    })
+    $("#documentation-tab .item .icon-upload").on("change", (e) => {
+        if (e.target.files.length > 0) {
+            let semaphore = $(e.target).closest("div.body").children(".semaphore");
+            semaphore.children().addClass("outline");
+            semaphore.children(".orange").removeClass("outline");
+        }
     })
 }
 
